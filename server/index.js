1d01dca112d01c97c67da0c767d68286d057e7c1
Gerard2167 <gerardm.2410@gmail.com>
2025-09-16 22:19:14 -0500

    fix: Reemplazar bcrypt por bcryptjs para compatibilidad

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // Cargar variables de entorno desde el archivo .env apropiado
